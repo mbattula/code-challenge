f69316fb63da30bc74863f177b0d667a52bdeaa5
Mahesh.Battula <mahesh.battula@herodigital.com>
2018-12-07 19:50:40 -0800

    code-challnge-v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter/Counter';
+import Button from './Button/Button';
+import DynamicText from './DynamicText/DynamicText';
+import Filter from './Filter/Filter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Code Challenge-1-Counter</h1>
+        <Counter />
+        <h1>Code Challenge-2-Scrolling</h1>
+        <DynamicText />
+        <h1>Code Challenge-3-List</h1>
+        <Filter />
       </div>
     );
   }
